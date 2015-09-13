c585ff8decc7471d06f6a5912517475bc57a3619
musicmunky <musicmunky@gmail.com>
2015-09-13 10:48:20 -0400

    Update with db schema and migration files

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150913135521) do
+
+  create_table "schedules", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.date     "schedule_date"
+    t.text     "notes",         limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "author",     limit: 255
+    t.string   "song_key",   limit: 255
+    t.integer  "capo_fret",  limit: 4
+    t.string   "media_link", limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "songs_schedules", id: false, force: :cascade do |t|
+    t.integer "song_id",     limit: 4
+    t.integer "schedule_id", limit: 4
+  end
+
+  add_index "songs_schedules", ["schedule_id"], name: "index_songs_schedules_on_schedule_id", using: :btree
+  add_index "songs_schedules", ["song_id"], name: "index_songs_schedules_on_song_id", using: :btree
+
+end
