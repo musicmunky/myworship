846aee31c3b5f35a4b7fa0a91872a85408efd824
musicmunky <musicmunky@gmail.com>
2015-09-18 10:12:01 -0400

    Update with date validation on schedule form

--- app/assets/javascripts/worship.js
+++ app/assets/javascripts/worship.js
@@ -88,7 +88,49 @@ function checkScheduleForm()
 		alert("Please enter both a name and a date before saving the schedule!");
 		return false;
 	}
+
+	if(!isValidDate(sdate))
+	{
+		alert("Please select a valid date!");
+		return false;
+	}
+
 	return true;
+}
+
+
+// Validates that the input string is a valid date formatted as "mm/dd/yyyy"
+function isValidDate(dateString)
+{
+	// First check for the pattern
+	if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+	{ return false; }
+
+	// Parse the date parts to integers
+	var parts = dateString.split("/");
+	var day   = 0;
+	var month = 0;
+	var year  = 0;
+
+	if(parts.length == 3)
+	{
+		day   = parseInt(parts[1], 10);
+		month = parseInt(parts[0], 10);
+		year  = parseInt(parts[2], 10);
+	}
+
+	// Check the ranges of month and year
+	if(year < 2000 || year > 2100 || month < 1 || month > 12)
+	{ return false; }
+
+	var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+	// Adjust for leap years
+	if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+	{ monthLength[1] = 29; }
+
+	// Check the range of the day
+	return day > 0 && day <= monthLength[month - 1];
 }
 
 
