5bf7d17bafdee317444eb17591aec042eec9cdf9
musicmunky <musicmunky@gmail.com>
2015-09-29 08:06:08 -0400

    Update to database and models with habtm relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable #:recoverable,
+	rolify
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+			:rememberable, :trackable, :validatable #:recoverable,
+	has_and_belongs_to_many :attendances
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926183812) do
+ActiveRecord::Schema.define(version: 20150929120014) do
+
+  create_table "attendance_users", id: false, force: :cascade do |t|
+    t.integer "user_id",       limit: 4
+    t.integer "attendance_id", limit: 4
+  end
+
+  add_index "attendance_users", ["attendance_id"], name: "index_attendance_users_on_attendance_id", using: :btree
+  add_index "attendance_users", ["user_id"], name: "index_attendance_users_on_user_id", using: :btree
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer  "user_id",     limit: 4
+    t.text     "description", limit: 65535
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name",          limit: 255
