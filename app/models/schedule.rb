88b318e50e430a2584e55d6d2531864d8a126405
musicmunky <musicmunky@gmail.com>
2015-10-22 14:23:22 -0400

    Update to highlight next schedule coming up

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,8 @@ class Schedule < ActiveRecord::Base
 	has_and_belongs_to_many :songs
 	serialize :song_order
 
+	scope :next_schedule, Proc.new { |after = DateTime.now, limit = 1| where('schedule_date > ?', after).order("schedule_date ASC").limit(limit) }
+
 	def get_songs_by_order
 		ids		= !self.song_order.nil? ? self.song_order.map(&:to_i) : []
 		@songs	=  ids.length > 0 ? Song.find(ids).index_by(&:id).values_at(*ids) : []
