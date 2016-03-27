86323c61b8fb5b85236d5d05a64cbac84b424c0b
musicmunky <musicmunky@gmail.com>
2016-03-27 09:14:19 -0400

    Update with passenger for server

--- db/migrate/20150913135521_create_song_schedule.rb
+++ db/migrate/20150913135521_create_song_schedule.rb
@@ -1,8 +1,8 @@
 class CreateSongSchedule < ActiveRecord::Migration
-  def change
-    create_table :schedules_songs, :id => false do |t|
-	t.belongs_to :song, index: true
-	t.belongs_to :schedule, index: true
-    end
-  end
+	def change
+		create_table :schedules_songs, :id => false do |t|
+			t.belongs_to :song, index: true
+			t.belongs_to :schedule, index: true
+		end
+	end
 end
