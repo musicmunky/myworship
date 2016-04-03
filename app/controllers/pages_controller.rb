bdb73dcb97f8037b3b12d5f820409061af2c61ec
musicmunky <musicmunky@gmail.com>
2016-04-03 12:20:39 -0400

    Update to FINALLY use the secrets file and stop tiptoeing around credentials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
 
 	def index
-#		@schedules = Schedule.all.order("schedule_date DESC")
-# 		@posts = Post.paginate(:page => params[:page], :per_page => 20)
+		require 'tweet_builder'
+
+		@tweetbld = TweetBuilder.new
+
+		@client = Twitter::REST::Client.new do |config|
+			config.consumer_key        = Rails.application.secrets.twitter_consumer_key
+			config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
+			config.access_token        = Rails.application.secrets.twitter_access_token
+			config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+		end
+
 		@schedules = Schedule.paginate(:page => params[:page], :per_page => 10).order("schedule_date DESC")
 	end
 
